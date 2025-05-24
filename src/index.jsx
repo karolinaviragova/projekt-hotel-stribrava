a8dabe20fb11e52764eb311dda7361ce0502f64f
Karolina Viragova <karolinavi94@gmail.com>
2025-05-24 17:17:12 +0200

    admin page

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
+import { AdminPage } from './pages/AdminPage';
 
 createRoot(
   document.querySelector('#app'),
-).render(<HomePage />);
+).render(<AdminPage />);
