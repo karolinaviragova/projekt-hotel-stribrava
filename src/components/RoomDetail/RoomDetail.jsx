03d514fed93c1257dd61d8cde5278d3f84d178a9
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-05-24 12:03:57 +0200

    Add RoomDetail and RoomList components

---
+++ src/components/RoomDetail/RoomDetail.jsx
@@ -0,0 +1,13 @@
+export const RoomDetail = () => {
+  return (
+    <div class="column">
+    <img src="img/image1.svg" />
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
+      accusantium, dolor quisquam doloremque quod nobis temporibus
+      ducimus sapiente consectetur distinctio assumenda, nisi suscipit
+      saepe. Vero.
+    </p>
+  </div>
+  )
+}
--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,45 +1,18 @@
+import { RoomList } from "../RoomList/RoomList";
+import { RoomDetail } from "../RoomDetail/RoomDetail";
+
+
+
 export const Rooms = () => {
   return (
     <div>
-      <section class="dark">
-        <div class="container">
-          <h2>Heading</h2>
-          <p>Quas odio quidem, enim nihil unde quia temporibus vitae in ab.</p>
-          <div class="cards-row">
-            <div class="card">
-              <img class="card__image" src="img/image1.svg" />
-              <div class="card__title">Card 1</div>
-              <div class="card__body">Sunt natus</div>
-            </div>
 
-            <div class="card">
-              <img class="card__image" src="img/image1.svg" />
-              <div class="card__title">Card 2</div>
-              <div class="card__body">Laboriosam</div>
-            </div>
-
-            <div class="card">
-              <img class="card__image" src="img/image1.svg" />
-              <div class="card__title">Card 3</div>
-              <div class="card__body">Eveniet officiis</div>
-            </div>
-          </div>
-        </div>
-      </section>
-
+    <RoomList />
       <section class="light">
         <div class="container">
           <h2>Heading</h2>
           <div class="columns-2">
-            <div class="column">
-              <img src="img/image1.svg" />
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
-                accusantium, dolor quisquam doloremque quod nobis temporibus
-                ducimus sapiente consectetur distinctio assumenda, nisi suscipit
-                saepe. Vero.
-              </p>
-            </div>
+           <RoomDetail />
             <form>
               <div class="form-fields">
                 <label htmlFor="field1" class="field-label">
