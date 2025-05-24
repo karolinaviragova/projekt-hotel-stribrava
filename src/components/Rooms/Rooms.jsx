62cc2ad9deba1c931654687f2eae199b5deb33f5
Karolina Viragova <karolinavi94@gmail.com>
2025-05-24 12:05:39 +0200

    new components Form

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,47 +1,17 @@
-import { RoomList } from "../RoomList/RoomList";
-import { RoomDetail } from "../RoomDetail/RoomDetail";
-
-
+import { RoomList } from '../RoomList/RoomList';
+import { RoomDetail } from '../RoomDetail/RoomDetail';
+import { Form } from '../Form/Form';
 
 export const Rooms = () => {
   return (
     <div>
-
-    <RoomList />
+      <RoomList />
       <section class="light">
         <div class="container">
           <h2>Heading</h2>
           <div class="columns-2">
-           <RoomDetail />
-            <form>
-              <div class="form-fields">
-                <label htmlFor="field1" class="field-label">
-                  Field 1:
-                </label>
-                <input id="field1" class="field-input" type="text" />
-
-                <label htmlFor="field2" class="field-label">
-                  Field 2:
-                </label>
-                <input id="field2" class="field-input" type="text" />
-
-                <label htmlFor="select" class="field-label">
-                  Select:
-                </label>
-                <select id="select" class="field-input">
-                  <option>Option 1</option>
-                  <option>Option 2</option>
-                  <option>Option 3</option>
-                  <option>Option 4</option>
-                </select>
-
-                <label htmlFor="check1" class="field-label">
-                  Checkbox 1:
-                </label>
-                <input id="check1" class="field-input" type="checkbox" />
-              </div>
-              <button class="wide">Submit</button>
-            </form>
+            <RoomDetail />
+            <Form />
           </div>
         </div>
       </section>
