394c3f00437108f221e6d3a595059650b372434f
Karolina Viragova <karolinavi94@gmail.com>
2025-05-24 11:31:26 +0200

    new component Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import hotelImg from "./hotel.jpg"
+
+export const Header = () => {
+  return (
+    <div class="banner">
+      <div class="banner__stripe">
+        <div class="container">
+          <h1>Hotel Stříbrava</h1>
+          <p class="lead">
+            Rodinný hotel v malebné krajině Českého středohoří.
+          </p>
+        </div>
+      </div>
+      <img src={hotelImg} alt="pohled na hotel stribrava" />
+    </div>
+  );
+};
