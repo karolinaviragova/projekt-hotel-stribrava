3d87fe42fb86f1b102d13be19b881f517d36e659
Karolina Viragova <karolinavi94@gmail.com>
2025-05-24 11:32:00 +0200

    Header added to homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,19 +1,11 @@
+import { Header } from '../../components/Header/Header';
+
 import './style.css';
 
 export const HomePage = () => {
   return (
     <div className="container">
-      <div class="banner">
-        <div class="banner__stripe">
-          <div class="container">
-            <h1>Template</h1>
-            <p class="lead">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
-              sequi.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Header />
 
       <section class="dark">
         <div class="container">
